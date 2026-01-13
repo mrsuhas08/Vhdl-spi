b70656e463d55edf609ee3816288dfaa589ffd45
mrsuhas08 <96473622+mrsuhas08@users.noreply.github.com>
2026-01-13 11:36:27 +0530

    Update and rename Slave.vhd to SPI_Slave.vhd

--- SPI_Slave.vhd
+++ SPI_Slave.vhd
@@ -1,17 +1,46 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 08.12.2025 16:55:45
+-- Design Name: 
+-- Module Name: spi_slave - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.numeric_std.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
 
 entity spi_slave is
     generic(addr_width  :   integer := 8;
             data_width  :   integer := 16);
     port(clk    :   in std_logic;
          rst    :   in std_logic;
-         sclk   :   in std_logic;
-         cs     :   in std_logic;
-         mosi   :   in std_logic;
+         sclk_s :   in std_logic;
+         cs_s   :   in std_logic;
+         mosi_s :   in std_logic;
          
-         miso   :   out std_logic);
+         miso_s :   out std_logic);
 end spi_slave;
 
 architecture Behavioral of spi_slave is
@@ -43,19 +72,19 @@ begin
     begin
     
         if rising_edge (clk)then
-            dsclk   <=  sclk;
+            dsclk   <=  sclk_s;
         end if;
         
     end process;
     
-    fedge   <=  (dsclk and (not sclk));
-    redge   <=  (sclk and (not dsclk));
+    fedge   <=  (dsclk  and (not sclk_s));
+    redge   <=  (sclk_s and (not dsclk));
     
     process(clk,rst)is
     begin
     
-        if cs = '1' then
-            miso    <=  'Z';
+        if cs_s = '1' then
+            miso_s  <=  'Z';
             reg_addr<=  (others => '0');
             shift_a <=  (others => '0');
             shift_d <=  (others => '0');
@@ -75,7 +104,7 @@ begin
                 when rw =>
                 
                     if fedge = '1' then
-                        shift_rw<=  mosi;
+                        shift_rw<=  mosi_s;
                         count   <=  0;
                         state   <=  addr;
                     end if;
@@ -85,23 +114,25 @@ begin
                     if fedge = '1' then
                     
                         if count < addr_width then
-                            shift_a <=  shift_a(addr_width-2 downto 0) & mosi;
+                            shift_a <=  shift_a(addr_width-2 downto 0) & mosi_s;
                             count   <=  count+1;
                             
                             if count = addr_width-1 then
-                                reg_addr<=  shift_a(addr_width-2 downto 0) & mosi;
+                                reg_addr<=  shift_a(addr_width-2 downto 0) & mosi_s;
                                 count   <=  0;
                                 
                                 if shift_rw = '1' then    --read
-                                    shift_d <=  mem(TO_INTEGER(unsigned(shift_a(addr_width-2 downto 0) & mosi)));
+                                    shift_d <=  mem(TO_INTEGER(unsigned(shift_a(addr_width-2 downto 0) & mosi_s)));
                                     count   <=  0;
                                     state   <=  rdata;
+                                    
                                 end if;
                                 
                                 if shift_rw = '0' then    --write
                                     shift_d <=  (others => '0');
                                     count   <=  0;
                                     state   <=  wdata;
+                                    
                                 end if;
                                 
                             end if;
@@ -115,16 +146,18 @@ begin
                     if fedge = '1' then
                     
                         if count >= 0 and count < data_width then
-                            shift_d <=  shift_d(data_width-2 downto 0) & mosi;
+                            shift_d <=  shift_d(data_width-2 downto 0) & mosi_s;
                             count   <=  count+1;
                             
                             if count = data_width-1 then
-                                mem(TO_INTEGER(unsigned(reg_addr))) <=  shift_d(data_width-2 downto 0) & mosi;
+                                mem(TO_INTEGER(unsigned(reg_addr))) <=  shift_d(data_width-2 downto 0) & mosi_s;
+                                
                             end if;
                             
                             if count = data_width then
                                 count   <=  0;
                                 state   <=  idle;
+                                
                             end if;
                             
                         end if;
@@ -135,14 +168,15 @@ begin
                 
                     if fedge = '1' then
                     
-                        if count >= 0 and count <data_width then
-                            miso    <=  shift_d(data_width-1);
+                        if count >= 0 and count < data_width then
+                            miso_s  <=  shift_d(data_width-1);
                             shift_d <=  shift_d(data_width-2 downto 0) & '0';
                             count   <=  count+1;
                             
                             if count = data_width then
                                 count   <=  0;
                                 state   <=  idle;
+                                
                             end if;
                             
                         end if;
